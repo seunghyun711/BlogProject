c8239d23c1e9a6894e1def8cf572a3b91f2908a7
seunghyun711 <seunghyun711@gmail.com>
2023-02-09 21:23:04 +0900

    http 요청 테스트1

---
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -0,0 +1,34 @@
+package com.cos.blog.test;
+
+import org.springframework.web.bind.annotation.*;
+
+// 사용자가 요청에 대한 데이터 응답을 해주는 컨트롤러 -> RestController
+@RestController
+public class HttpControllerTest {
+    // get,post,put,delete 테스트
+
+    // 브라우저로 요청하는 것은 get만 가능하다 나머지는 postman 같은 툴로 확인해야 한다.
+    // http://localhost:8080/http/get
+    @GetMapping("/http/get")
+    public String getTest() {
+        return "get 요청";
+    }
+
+    // http://localhost:8080/http/post
+    @PostMapping("/http/post")
+    public String postTest() {
+        return "post 요청";
+    }
+
+    // http://localhost:8080/http/put
+    @PutMapping("/http/put")
+    public String putTest() {
+        return "put 요청";
+    }
+
+    // http://localhost:8080/http/delete
+    @DeleteMapping("/http/delete")
+    public String deleteTest() {
+        return "delete 요청";
+    }
+}
