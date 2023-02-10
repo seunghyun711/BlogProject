925de97186f99bab75d7e3e974d541fbf7f1c622
seunghyun711 <seunghyun711@gmail.com>
2023-02-10 19:25:09 +0900

    lombok 적용

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -7,18 +7,31 @@ import org.springframework.web.bind.annotation.*;
 public class HttpControllerTest {
     // get,post,put,delete 테스트
 
+    private static final String TAG = "HttpControllerTest";
+
+    @GetMapping("/http/lombok")
+    public String lombokTest(){
+//        Member m = new Member(1,"asda","1234","Asdgwe");
+        Member m = Member.builder().username("hong").password("1234").email("sadwlih@aaa").build();
+        System.out.println(TAG + "getter : " + m.getId());
+        m.setId(5000);
+        System.out.println(TAG + "setter : " +  m.getId());
+        return "lombok Test Compelte";
+
+    }
+
     // 브라우저로 요청하는 것은 get만 가능하다 나머지는 postman 같은 툴로 확인해야 한다.
     // http://localhost:8080/http/get
     @GetMapping("/http/get")
     public String getTest(Member m) { // http://localhost:8080/http/get?id=1 으로 요청을 보내면 @RequestParam으로 1을 받는다.
         // @RequestParma으로 받아올때는 id, username을 매개변수로 하나씩 받아왔지만 클래스를 넘겨받으면 그럴 필요 없다.
-        return "get 요청 : " + m.getId() + m.getUsername();
+        return "get 요청 : " + m.getId() + ", " +  m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
     }
 
     // http://localhost:8080/http/post
     @PostMapping("/http/post")
-    public String postTest(@RequestBody String text) {
-        return "post 요청 : " + text;
+    public String postTest(@RequestBody Member m) {
+        return "post 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
     }
 
     // http://localhost:8080/http/put
--- src/main/java/com/cos/blog/test/Member.java
+++ src/main/java/com/cos/blog/test/Member.java
@@ -1,5 +1,11 @@
 package com.cos.blog.test;
 
+import lombok.*;
+
+@Data // getter,setter동시 사용 가능
+//@AllArgsConstructor // 생성자 만들어진다.
+//@RequiredArgsConstructor // final이 붙은 변수에 대한 생성자를 생성한다.
+@NoArgsConstructor // 기본 생성자
 public class Member {
     /*
     아래 변수들을 private으로 선언하여 해당 변수들에 직접 접근하지 않게하고, 이를 이용하는 메서드를 public으로 만들어 메서드를 통해서만
@@ -10,42 +16,12 @@ public class Member {
     private String password;
     private String email;
 
-    public Member(int id, String username, String password, String email) {
+    @Builder // Member m = Member.builder().username("hong").password("1234").email("sadwlih@aaa").build();의 형식으로 객체 생성할 수 있다.
+    // builder패턴을 만들어준다. -> 값을 넣는 순서를 맞추지 않아도 된다. 순서화 필요 x 생성자를 쓸 때는 순서를 맞춰줘야 했다.
+    public Member(int id, String username, String password, String email) { // @AllArgsConstructor와 같은 동작을 한다.
         this.id = id;
         this.username = username;
         this.password = password;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
     }
 }
