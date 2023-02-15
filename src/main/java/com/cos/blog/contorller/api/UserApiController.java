b49c6f9fe9b661f9410638eb707cbfb7b6bcb247
seunghyun711 <seunghyun711@gmail.com>
2023-02-15 21:03:20 +0900

    UserService, 회원가입을 하나의 트랜잭션으로 설정

--- src/main/java/com/cos/blog/contorller/api/UserApiController.java
+++ src/main/java/com/cos/blog/contorller/api/UserApiController.java
@@ -1,7 +1,10 @@
 package com.cos.blog.contorller.api;
 
 import com.cos.blog.dto.ResponseDto;
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
+import com.cos.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,9 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserApiController {
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/blog/api/user")
     public ResponseDto<Integer> save(@RequestBody User user) {
         System.out.println("UserApiController : save 호출됨");
-        return new ResponseDto<Integer>(HttpStatus.OK,1);
+        user.setRole(RoleType.USER);
+        int result = userService.회원가입(user);
+        return new ResponseDto<Integer>(HttpStatus.OK,result);
     }
 }
---
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -0,0 +1,28 @@
+package com.cos.blog.service;
+
+import com.cos.blog.model.User;
+import com.cos.blog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service // 스프링이 컴포넌트 스캔을 통해 스프링 빈에 등록함 -> ioc해준다.
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional // 회원가입 메서드가 하나의 트랜잭션으로 묶임, 전체가 성공하면 커밋이 될 것이고, 실패 시 롤백이 될 것이다.
+    public int 회원가입(User user){
+        try{
+            userRepository.save(user);
+            return 1;
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("UserService : 회원가입() : " + e.getMessage());
+        }
+        return -1;
+    }
+
+}
