68959ad941e32531f7d4a6fdb77dc449e79d3410
seunghyun711 <seunghyun711@gmail.com>
2023-02-25 10:58:17 +0900

    user.js 수정

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -5,6 +5,9 @@ let index = {
         $("#btn-save").on("click",()=>{ // =>는 this를 바인딩하기 위해 사용
             this.save();
         });
+        $("#btn-update").on("click",()=>{ // =>는 this를 바인딩하기 위해 사용
+            this.update();
+        });
 
     },
 
@@ -34,7 +37,32 @@ let index = {
         }).fail(function(error){ // 응답의 결과가 실패하면 실행
             alert(JSON.stringify(error));
         }); // ajax 통신을 이용해 3개의 데이터를 json으로 변경하여 insert 요청
-    }
+    },
+
+    update: function(){
+            //alert('user의 save함수 호출됨');
+            let data = {
+                id: $("#id").val(),
+                username: $("username").val(),
+                password: $("#password").val(),
+                email: $("#email").val()
+            }
+
+            $.ajax({
+                type: "PUT",
+                url: "/user",
+                data: JSON.stringify(data), // http body데이터
+                contentType: "application/json; charset=utf-8", // body 데이터가 어떤 타입인지(MIME)
+                dataType: "json" // 요청을 서버로 해서 응답이 왔을 때 기본적으로 모든 것이 String(생긴게 json이라면) => javascript 객체로 변경해준다.
+            // 회원가입 수행 요창
+            }).done(function(resp){ // 응답의 결과가 성공이면 실행
+                alert("회원수정이 완료되었습니다.")
+    //            console.log(resp);
+                location.href = "/";
+            }).fail(function(error){ // 응답의 결과가 실패하면 실행
+                alert(JSON.stringify(error));
+            }); // ajax 통신을 이용해 3개의 데이터를 json으로 변경하여 insert 요청
+        }
 }
 
 index.init();
