81053380d59935fcc3892ec023fd6727e6295a8f
seunghyun711 <seunghyun711@gmail.com>
2023-02-25 16:26:25 +0900

    카카오 로그인 구현

--- src/main/java/com/cos/blog/contorller/UserController.java
+++ src/main/java/com/cos/blog/contorller/UserController.java
@@ -1,7 +1,15 @@
 package com.cos.blog.contorller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 // 인증이 안된 사용자들이 출입할 수 있는 경로를 /auth/** 이걸로만 허용
 // 그냥 주소가 /이면 index.html 허용
@@ -12,8 +20,38 @@ public class UserController {
 
     @GetMapping("/auth/joinForm") // 가입 폼으로 이동
     public String joinForm(){
-
         return "user/joinForm";
+    }
+
+    @GetMapping("/auth/kakao/callback")
+    public @ResponseBody String kakaoCallback(String code){ // 데이터를 리턴해주는 컨트롤러 주소
+        // Post 방식으로 key=value 데이터를 요청(카카오로)
+        RestTemplate rt = new RestTemplate();
+
+        // HttpHeader 오브젝트 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type","application/x-www-form-urlencoded;charset=utf-8");
+
+        // HttpBody 오브젝트 생성
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type","authorization_code");
+        params.add("client_id","d6f38781580c15aa9995e87350867979");
+        params.add("redirect_uri","http://localhost:8080/auth/kakao/callback");
+        params.add("code",code);
+
+        // HttpHeader와 HttpBody로 하나의 오브젝트에 담기
+        HttpEntity<MultiValueMap<String,String>> kakaoTokenRequest =
+                new HttpEntity<>(params,headers);
+
+        // Http 요청하기 - POST 방식으로 그리고 response 변수의 응답 받음
+        ResponseEntity<String> response = rt.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        return response.getBody();
     }
 
     @GetMapping("/auth/loginForm") // 로그인 폼으로 이동
