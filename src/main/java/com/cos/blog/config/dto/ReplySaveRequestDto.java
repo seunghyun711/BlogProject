8b58f9beea3a7fce722a2123d63057c59d249fee
seunghyun711 <seunghyun711@gmail.com>
2023-02-26 18:45:26 +0900

    댓글 dto 사용

---
+++ src/main/java/com/cos/blog/config/dto/ReplySaveRequestDto.java
@@ -0,0 +1,14 @@
+package com.cos.blog.config.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReplySaveRequestDto {
+    private Long userId;
+    private Long boardId;
+    private String content;
+}
